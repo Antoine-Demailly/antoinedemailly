8b6b6c41ca92f2e30d36f6f543c909112d946801
Antoine-Demailly <antoine.demailly@gmx.fr>
2015-08-09 14:22:45 +0200

    New structure in progress

--- src/Website/Controller/HomeController.php
+++ src/Website/Controller/HomeController.php
@@ -6,11 +6,11 @@ class HomeController {
 
 	public function homeAction() {
 		return [
-    		'html_view' => '/basic.structure.phtml',
+    		'html_view' => '/basic/structure.phtml',
     		'html_view_dynamic' => '/home.dynamic.phtml',
     		'page_author' => 'Antoine Demailly',
-    		'page_description' => 'Connexion à l\'interface d\'administration de Trottilib.fr',
-    		'page_title' => 'Trottilib - Interface d\'adminisration - Login' 
+    		'page_description' => '',
+    		'page_title' => 'Antoine Demailly - Développeur Web' 
     	];
 	}
 
