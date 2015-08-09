b0bcb7ff62c8070b895137524e4fce39bf585a8e
Antoine-Demailly <antoine.demailly@gmx.fr>
2015-08-09 13:05:50 +0200

    Edit structure

--- src/Website/Controller/HomeController.php
+++ src/Website/Controller/HomeController.php
@@ -6,7 +6,8 @@ class HomeController {
 
 	public function homeAction() {
 		return [
-    		'html_view' => 'home.phtml',
+    		'html_view' => '/basic.structure.phtml',
+    		'html_view_dynamic' => '/home.dynamic.phtml',
     		'page_author' => 'Antoine Demailly',
     		'page_description' => 'Connexion à l\'interface d\'administration de Trottilib.fr',
     		'page_title' => 'Trottilib - Interface d\'adminisration - Login' 
